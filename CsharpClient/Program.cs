b23edc8bf45cf17ae5f01ff89cc8b64e2461d946
thenikkoman <nikolas_met@hotmail.com>
2012-11-17 00:45:47 +0100

    General code cleanup, added a TODO, added non-working writing to server, fixed a bug where the port could only be 27000 (still untested; should work in theory).

--- CsharpClient/Program.cs
+++ CsharpClient/Program.cs
@@ -1,15 +1,59 @@
 ﻿using System;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
 using System.Threading;
 
+// TODO:
+// Shut down cleanly. Access the NetworkStream stream from the keylistener thread, and .Shutdown and .Disconnect
+// Send messages back to the server
+
 namespace Client
 {
-    class Program
+    public class Program
     {
         public static bool exit;
+        public static NetworkStream stream;
+        public static StreamWriter sw;
+
+        public static void sendToServer(string fuckedMessage)
+        {
+            Console.WriteLine("fuckedMessage is: " + fuckedMessage);
+            string[] Almostmessage = fuckedMessage.Split( ' ' );
+            string message = Almostmessage[1];
+            Console.WriteLine("message is: " + message);
+            sw.WriteLine(message);
+            sw.Flush();
+        }
+
+        public static void ListerKeyBoardEvent()
+        {
+            string ConsoleInput;
+            while (true)
+            {
+                ConsoleInput = Console.ReadLine();
+                string lowerconsoleinput = Console.ReadLine().ToLower();
+                if ((lowerconsoleinput == "quit") || (lowerconsoleinput == "exit"))
+                {
+                    exit = true;
+                }
+                else if (lowerconsoleinput.StartsWith("send "))
+                {
+                    sendToServer(ConsoleInput);
+                }
+                else if (lowerconsoleinput == "help")
+                {
+                    Console.WriteLine("The available commands are:\nhelp: Opens this list.\nexit: see quit.\nquit: Closes the consolewindow, killing the client.");
+                }
+                else
+                {
+                    Console.WriteLine("Unknown command. Try the 'help' command for a list of commands");
+                }
+            }
+        }
+
         public static void Main(string[] args)
         {
             int localPort = 27000;
@@ -42,14 +86,14 @@ namespace Client
                 PortString = "27000";
                 localPort = 27000;
             }
+            else if (!(Int32.TryParse(PortString, out localPort)))
+            {
+                Console.WriteLine("The port you specified was invalid. Please enter it again.");
+                goto connectPort;
+            }
             else
             {
-
-                if (!(Int32.TryParse(PortString, out localPort)))
-                {
-                    Console.WriteLine("The port you specified was invalid. Please enter it again.");
-                    goto connectPort;
-                }
+                localPort = Int32.Parse(PortString);
             }
             byte[] ByteBuffer = new byte[bufferSize];
             try
@@ -62,6 +106,7 @@ namespace Client
                 goto connectHost;
             }
             NetworkStream stream = listener.GetStream();
+            StreamWriter streamwriter = new StreamWriter(stream);
             Thread ConsoleKeyListener = new Thread(new ThreadStart(ListerKeyBoardEvent));
             Console.WriteLine("\nType help for a list of commands.");
             ConsoleKeyListener.Start();
@@ -73,35 +118,7 @@ namespace Client
                 Console.WriteLine("The bytes recieved are: " + BitConverter.ToString(ByteBuffer.Reverse().SkipWhile(x => x == 0).Reverse().ToArray()));
                 Array.Clear(ByteBuffer, 0, bufferSize);
             }
-        }
-
-        public static void ListerKeyBoardEvent()
-        {
-            string ConsoleInput;
-            Program threadlistener = new Program();
-            while (true)
-            {
-                ConsoleInput = Console.ReadLine();
-                switch (ConsoleInput.ToLower())
-                {
-                    case "quit":
-                        Console.WriteLine("You reached the exit-block");
-                        threadlistener.Client.Disconnect(false);
-                        Console.WriteLine("You should be disconnected now");
-                        Environment.Exit(0);
-                        break;
-                    case "exit":
-                        goto case "quit";
-                    case "help":
-                        Console.WriteLine("The available commands are:\nhelp: Opens this list.\nexit: see quit.\nquit: Closes the consolewindow, killing the client.");
-                        break;
-                    //case (StartsWith("send")):
-
-                    default:
-                        Console.WriteLine("Unknown command. Try the 'help' command for a list of commands'");
-                        break;
-                }
-            }
+            Console.WriteLine("Success! You should be able to exit now!"); // Environment.Exit(0);
         }
     }
 }
